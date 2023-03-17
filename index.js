3ddb3ce7f43e02ae6d57d2ca137093029d79f66e
AlfieVidrio <vidrio@uw.edu>
2023-03-16 20:07:57 -0700

    press 'enter' to search

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import {
   render
 } from "lit-html";
 
-let searchButton = document.getElementById("search")
-searchButton.addEventListener("click", doSearch)
-
-
+let searchBar = document.getElementById("search")
+searchBar.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    // Execute search function with search term
+    console.log("passed search query=", searchBar.value)
+    doSearch();
+  }
+});
 function doSearch() {
-  console.log("searching!")
-  let searchStr = document.getElementById("text-input").value;
-  console.log(searchStr)
-
+  console.log("executing search!")
+  let searchStr = searchBar.value;
   let baseURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
   let newURL = baseURL.concat(searchStr)
   console.log(newURL)
@@ -28,12 +30,18 @@ function doSearch() {
 
       // Use .map to create an array of html templates to render to the DOM
       let drinkList = drinks.drinks.map(function(drink) {
-        return html`<div class="user-entry">
-      <h2>${drink.strDrink}</h2>
-    </div>`;
+        console.log("test 1:", drink)
+        return html`<div @click=${()=>renderRecipe(drink)} class="user-entry">
+        <h2>${drink.strDrink}</h2>
+      </div>`;
       });
 
       // Render the userList array to the user-list div
       render(drinkList, document.getElementById("user-list"));
     });
 }
+
+function renderRecipe(drink) {
+  console.log("test 2:", drink)
+  render(drink.strGlass, document.getElementById("recipe"));
+} 
